8b9e0a2fa531eb9bb2e780b0089569efc51b84c9
Jonas Kirk Pedersen <jonas.pedersen@ufst.dk>
2020-10-19 16:42:41 +0200

    code covage startet and tests added. New Readme

--- README.md
+++ README.md
@@ -6,7 +6,10 @@ Kræver pip pakker:
 - ansible
 - django-extensions https://django-extensions.readthedocs.io/en/latest/
 - django-environg https://django-environ.readthedocs.io/en/latest/
-
+- pytest
+- pytest-django
+- pytest-cov
+- unittest
 
 ## Environmental file .env
 Denne fil indholder env variabler til bruge i django installationen.
@@ -19,3 +22,18 @@ Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 I .env file vil det derfor have input som:
 - DEBUG=TRUE
 - SECRET_KEY=2F3fkdfu3e9fe>JJF........fFJEFEF
+
+
+
+## Pytest 
+
+Python test binary
+
+I pytest.ini er der defineret settings til brug for test. En lokal settings file.
+
+```python
+[pytest]
+DJANGO_SETTINGS_MODULE = minitower.local_settings
+
+python_files = tests.py test_*.py *_test.py
+```
--- inventory/tests.py
+++ inventory/tests.py
@@ -10,6 +10,11 @@ def setup_inventory_table(classArg, host='localhost', ):
     hosts.objects.create(hostname = host)
     return Job_update.execute(classArg)
 
+
+class value_match:
+    def __init__(self, value):
+        self.value = value
+
 class TestInventoryUpdater(TestCase):
 
 
@@ -37,7 +42,7 @@ class TestInventoryValues(TestCase):
         setup_inventory_table(self)
         self.assertEqual(type(Job_inventory.get_server_facts(self, 'localhost')), dict)
     
-    def test_get_expressions(self):
+    def test_get_expressions_linux(self):
         """
         docstring
         """
@@ -45,12 +50,52 @@ class TestInventoryValues(TestCase):
         setup_inventory_table(self)
         self.assertEqual(type(Job_inventory.get_fact_expressions(self, 'Linux')), list)
 
-    def test_count_expressions(self):
+    def test_count_expressions_linux(self):
 
         list_elements = 6
         setup_inventory_table(self)
         
         self.assertEqual(len(Job_inventory.get_fact_expressions(self, 'Linux')), list_elements)
+
+    def test_get_expressions_windows(self):
+        """
+        docstring
+        """
+
+        setup_inventory_table(self)
+        self.assertEqual(type(Job_inventory.get_fact_expressions(self, 'Windows')), list)
+
+    def test_count_expressions_windows(self):
+
+        list_elements = 5
+        setup_inventory_table(self)
+        
+        self.assertEqual(len(Job_inventory.get_fact_expressions(self, 'Windows')), list_elements)
+
+    def test_update_db_linux(self):
+
+        setup_inventory_table(self)
+
+        match_count = 0
+        match = [1,'1.2.3.4',8,'redhat', '8.2', 'Linux']
+        system = 'Linux'
+        host = 'localhost'
+        expressions = [
+            'memtotal_mb',
+            'default_ipv4.address',
+            'processor_count', 
+            'os_family', 
+            'distribution_version', 
+            'system', 
+            ]
+
+        
+        for test in range(len(expressions)):
+            match_value = value_match([match[match_count]])
+            print(match_value[0].value)
+            match_count += 1
+
+            #Job_inventory.update_inventory_db(self, match, system, host, expressions[test])
 
 if __name__ == '__main__':
     unittest.main()
