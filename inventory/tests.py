a620297cb884c216de9856dff4c35630c715cb0f
Jonas Kirk Pedersen <jonas.pedersen@ufst.dk>
2020-10-19 11:51:06 +0200

    tests for update and retrievel of facts / expres

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,21 +1,56 @@
 from django.test import TestCase
 import unittest
 from inventory.models import Inventory
-from inventory.jobs.minutely.update_inventory_table import Job
-class TestInventoryValues(TestCase):
+from hosts.models import hosts
+from inventory.jobs.minutely.update_inventory_table import Job as Job_update
+from inventory.jobs.hourly.update_inventory import Job as Job_inventory
 
 
-    def test_get_host(self):
+def setup_inventory_table(classArg, host='localhost', ):
+    hosts.objects.create(hostname = host)
+    return Job_update.execute(classArg)
+
+class TestInventoryUpdater(TestCase):
+
+
+    def test_add_hostname(self):
         """
         docstring
         """
-        a = 1
-        b = 2
+        hosts.objects.create(hostname='localhost')
+    
+        self.assertWarnsMessage("Added 1 hosts to inventory tabel",Job_update.execute(self))
 
-        c = a + b
+    def test_present_host(self):
+        """
+        docstring
+        """
+        text = setup_inventory_table(self)
+        self.assertWarnsMessage(text,Job_update.execute(self))
 
-        self.assertEqual(c, 3)
+class TestInventoryValues(TestCase):
 
+    def test_present_host(self):
+        """
+        docstring
+        """
+        setup_inventory_table(self)
+        self.assertEqual(type(Job_inventory.get_server_facts(self, 'localhost')), dict)
+    
+    def test_get_expressions(self):
+        """
+        docstring
+        """
+
+        setup_inventory_table(self)
+        self.assertEqual(type(Job_inventory.get_fact_expressions(self, 'Linux')), list)
+
+    def test_count_expressions(self):
+
+        list_elements = 6
+        setup_inventory_table(self)
+        
+        self.assertEqual(len(Job_inventory.get_fact_expressions(self, 'Linux')), list_elements)
 
 if __name__ == '__main__':
     unittest.main()
