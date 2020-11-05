e551b95e0f9f355d90b9cd27a89eb6155f811e3b
Jonas Kirk Pedersen <jonas.pedersen@ufst.dk>
2020-11-05 14:09:54 +0100

    Inventory model updated to match value <20

--- inventory/models.py
+++ inventory/models.py
@@ -9,8 +9,8 @@ class Inventory(models.Model):
     cores = models.IntegerField('Number of cores')
     ram = models.IntegerField('Amount of memory')
     storage = models.CharField('Total space in Giga as a list', max_length=120)
-    os_family = models.CharField('OS family', max_length=20, default='RedHat')
-    os_version = models.CharField('OS version', max_length=20, default='8')
+    os_family = models.CharField('OS family', max_length=50, default='RedHat')
+    os_version = models.CharField('OS version', max_length=50, default='8')
     system = models.CharField('OS Type', max_length=20, default='Linux')
 
     def __str__(self):
