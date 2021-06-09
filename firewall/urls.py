6bdd46878d92bd0975c2e87f7457ed3b646eac06
Jonas Kirk Pedersen <jonas.pedersen@ufst.dk>
2021-06-09 09:13:14 +0200

    ?

--- firewall/urls.py
+++ firewall/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('firewall/', views.get_firewall_list, name = 'get_firewall_list'),
     re_path(r'([\w|\W]+)/add-firewall/', views.add_fw_create, name = 'add_fw_create') ,
-    path('add-firewall/', views.add_fw_create, name = 'add_fw_create')
-
+    path('add-firewall/', views.add_fw_create, name = 'add_fw_create'),
+    path('create/', views.AddModalFirewallView.as_view(), name='create_firewall_rule'),
+    path('fwrule/<int:id>', views.get_fwrule, name='get_fwrule')
 ]
--- firewall/views.py
+++ firewall/views.py
@@ -1,13 +1,37 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import firewall
+from django.urls import reverse_lazy
 
-from django.db.models import Q, Count
+from bootstrap_modal_forms.generic import BSModalCreateView
+
+from django.db.models import Q
 
 from .forms import AddFirewall
+from .forms import AddModalFirewall
+
+from .models import firewall
+
 
 def is_valid_queryparam(param):
     return param != '' and param is not None
+
+def get_fwrule(request, id):
+    """
+    docstring
+    """
+    qs = firewall.objects.all()
+    qs = qs.filter(pk__iexact=id)
+    ips = qs.filter(pk__iexact=id).values('sourcenat', 'destnat')
+    print(str(qs.query))
+    print(str(ips.query))
+    for key in ips:
+        print(f"{key}")
+    context ={
+        'fwset': qs
+    }
+
+    return render(request, 'firewall/fwrule.html', context )
 
 
 def get_firewall_list(request):
@@ -28,7 +52,7 @@ def filter(request):
             Q(source__icontains=fw_query)                       |
             Q(sourcenat__icontains=fw_query)                    |
             Q(dest__icontains=fw_query)                         |
-            Q(destnat__icontains=fw_query)              |
+            Q(destnat__icontains=fw_query)                      |
             Q(port__icontains=fw_query)                         |
             Q(oldfwid__icontains=fw_query)                      |
             Q(fwid__icontains=fw_query)                         |
@@ -54,3 +78,9 @@ def add_fw_create(request, *args, **kwargs):
         "form" : form
     }
     return render(request, "firewall/add_firewall.html", context)
+
+class AddModalFirewallView(BSModalCreateView):
+    template_name = 'firewall/add_firewallmodal.html'
+    form_class = AddModalFirewall
+    success_message = 'Success: Firewall rule was created.'
+    success_url = reverse_lazy('index')
